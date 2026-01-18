4bfc0ebb5bb7c1e3c6d974d40765d510b920f2ec
jjjjjjulik <julgab2309@gmail.com>
2026-01-19 01:21:15 +0200

    feat: Add initial UI components for todo list functionality, including forms, buttons, fields, and task display.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,7 +9,7 @@ const TodoList = () => {
 
   return (
     <ul className="todo__list">
-      <TodoItem />
+      <TodoItem title="Купить молоко" isDone={false} />
       <li className="todo__item todo-item">
         <input className="todo-item__checkbox" id="task-2" type="checkbox" />
         <label className="todo-item__label" htmlFor="task-2">
